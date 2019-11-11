9a23503e1e4b30c60af3d957966e53ab01824d3a
xoyabc <1031138448@qq.com>
2019-11-11 09:07:25 +0800

    add request to page 2 to last

--- search_movie_maccms.py
+++ search_movie_maccms.py
@@ -3,25 +3,25 @@
 import requests
 import sys
 import re
+import json
 from bs4 import BeautifulSoup
 
-try:
-    name = sys.argv[1].decode('utf-8')
-    
-except Exception as e:
-    print (e)
+'''
+1，多页：教父
+2，一页: 某种女人
+'''
+
+#try:
+#    name = sys.argv[1].decode('utf-8')
+#    
+#except Exception as e:
+#    print (e)
 
 url = "http://ys.louxiaohui.com/index.php"
-base_url = "https://ys.louxiaohui.com"
-title = u'{}'.format(name)
-keyboard = title.encode('utf-8')
+base_url = "http://ys.louxiaohui.com"
 
 # 请求参数
 querystring = {"m":"vod-search"}
-
-# 请求数据
-#payload = "wd=%E7%96%BE%E9%80%9F%E5%A4%87%E6%88%98"
-payload = "wd={}".format(keyboard)
 
 
 # 定义 header
@@ -39,24 +39,87 @@ headers = {
     }
 
 
+# 生成短链
+def gen_short_url(long_url):
+    url = 'http://tools.aeink.com/tools/dwz/urldwz.php'
+    payload = { 'api': 'urlcn', 'longurl': long_url}
+    try:
+        resp = requests.get(url, params=payload)
+        json_data = json.loads(resp.text)
+        short_link = json_data['ae_url']
+        return short_link
+    except Exception as e:
+        short_link = None
+    
+
+def get_movie_info(all_result, movie_year):
+    for i in all_result:
+        link = i.select('div[class="l"] > h2 > a')[0]['href'].encode('utf-8').strip()
+        real_link = base_url + link
+        tiny_link = gen_short_url(real_link)
+        name = i.select('div[class="l"] > h2 > a')[0].text.encode('utf-8').strip()
+        year_anchor = i.select('div[class="l"]')[0].find("span", text=re.compile("年份：".decode("utf-8")))
+        year = year_anchor.next_element.next_element.encode('utf-8').strip()
+        #if name == keyword and int(movie_year) == int(year):
+        #if name == keyword:
+        if not movie_year:
+            if name == keyword:
+                print ("{} {} {}".format(name, real_link, tiny_link))
+        else:
+            if name == keyword and int(movie_year) == int(year):
+                print ("{} {} {} {}".format(name, year, real_link, tiny_link))
+
+
 # 搜索函数
-def search_maccms():
+def search_maccms(name, year = None):
+    print name,year
+    global keyword
+    title = u'{}'.format(name.decode('utf-8'))
+    #keyword = quote(title.encode('utf-8'))
+    keyword = name.decode('utf-8').encode('utf-8')
+    # 请求数据
+    #payload = "wd=%E7%96%BE%E9%80%9F%E5%A4%87%E6%88%98"
+    payload = "wd={}".format(keyword)
+    print payload, querystring
     response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
     html = response.text
     soup = BeautifulSoup(html.encode('utf-8'), "html.parser")
-    all_result = soup.select('div[class="channel b"] > ul > li')[0:2]
+    all_result = soup.select('div[class="channel b"] > ul > li')[0:10]
     
     if len(all_result) > 0:
-        for i in all_result:
-            link = i.select('div[class="l"] > h2 > a')[0]['href'].strip()
-            real_link = base_url + link
-            name = i.select('div[class="l"] > h2 > a')[0].text.encode('utf-8').strip()
-            anchor = i.select('div[class="l"]')[0].find("span", text=re.compile("年份：".decode("utf-8")))
-            year = anchor.next_element.next_element.strip()
-            print ("{} {} {}".format(name, year, real_link))
-    else:
+        get_movie_info(all_result, year)
+    elif len(all_result) == 0:
         print ("未找到相关影片，请更换关键词")
+    # 翻页
+    if len(all_result) == 10:
+        NEXT_PAGE = True
+        try:
+            next_page_link = soup.find("a", class_="pagelink_a", text=re.compile("下一页".decode("utf-8")))['href']
+            real_next_page_link = base_url + next_page_link
+        except Exception as e:
+            NEXT_PAGE = False
+        while NEXT_PAGE:
+            resp = requests.get(real_next_page_link)
+            print real_next_page_link
+            print (resp)
+            html = resp.text
+            soup = BeautifulSoup(html.encode('utf-8'), "html.parser")
+            all_result = soup.select('div[class="channel b"] > ul > li')[0:10] 
+            get_movie_info(all_result, year)
+            try:
+                next_page_link = soup.find("a", class_="pagelink_a", text=re.compile("下一页".decode("utf-8")))['href']
+                real_next_page_link = base_url + next_page_link
+            except Exception as e:
+                NEXT_PAGE = False
+
+
+def search_by_movie_name():
+    with open('movie.name', 'rU') as f:
+        for line in f:
+            name = line.split()[0].strip()
+            print name
+            search_maccms(name)
 
 
 if __name__ == '__main__':
-    search_maccms()
+    search_by_movie_name()
